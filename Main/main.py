353eebcf4fdcd8fa086ea96521e59c18cb87b3ed
itzhapp <88381788+itzhapp@users.noreply.github.com>
2021-10-11 22:50:23 +0200

    Update main.py to be Object Oriented
    Added Functions:
    Main(), tryPortRange(), tryPort()
    
    Added Functionality:  To scan in a specific  portrange and the option to just scan one port.

--- Main/main.py
+++ Main/main.py
@@ -1,17 +1,52 @@
 
 import socket
 
-#Input information about IP and Port which is going to be used to try to connect to target
-SERVER = input("Target IP: ")
-PORT = int(input("Target Port: "))
-ADDR= (SERVER,PORT)
+def tryPort():
+	#Input information about IP and Port which is going to be used to try to connect to target
+	SERVER = input("Target IP: ")
+	PORT = int(input("Target Port: "))
+	ADDR= (SERVER,PORT)
 
-#Creating a client which requests services 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	#Creating a client which requests services 
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-try: 
-	#Try to connect to the Server at the given port
-	client.connect(ADDR)
-	print("Successfully detected open port!")
-except:
-	print("Failed to connect to server...")
+	try: 
+		#Try to connect to the Server at the given port
+		client.connect(ADDR)
+		print("Successfully detected open port!")
+	except:
+		print("Failed to connect to server...")
+
+def tryPortRange():
+	PORTSTART = int(input("Target Start Port: "))
+	PORTEND = int(input("Target End Port:"))
+
+	portdata = []
+	#Input information about IP and Port which is going to be used to try to connect to target
+	SERVER = input("Target IP: ")
+
+	#Creating a client which requests services 
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	for port in range(PORTSTART,PORTEND):
+		ADDR = (SERVER,int(port))
+		try: 
+			#Try to connect to the Server at the given port
+			client.connect(ADDR)
+			portdata.append(port)
+		except:
+			None
+	print("Found Ports: ")
+	for i in portdata:
+		print(i)
+
+def main():
+	multipleports = input("Portrange? Y/N: ")
+	if multipleports == "N":
+		tryPort()
+
+	if multipleports == "Y":
+		tryPortRange()
+
+if __name__ == "__main__":
+    main()
